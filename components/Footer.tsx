40ae20dc9a1e736c923ed75df05f7e911b00997b
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-11-20 13:01:06 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,19 +4,48 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='border-t border-white mt-40 p-10 text-white flex flex-col items-center'>
-        <p className='mb-4'>arcade@example.com</p>
-        <div className='inline-flex gap-4 mb-4'>
-            <Image src="/instagram.svg" alt='Icon'
-            width={25} height={25} className='cursor-pointer'></Image>
-            <Image src="/facebook.svg" alt='Icon'
-            width={25} height={25} className='cursor-pointer'></Image>
-            <Image src="/reddit.svg" alt='Icon'
-            width={25} height={25} className='cursor-pointer'></Image>
+    <footer className='footer border-t border-white mt-[13rem] p-[3rem]'>
+        <div className='flex justify-between items-center text-white'>
+          <Link href="/" className='hover:underline decoration-2 underline-offset-2 mb-3'>GAMES</Link>
+          <Link href="/" className='hover:underline decoration-2 underline-offset-2 mb-3'>ABOUT AND CONTACT</Link>
+
+         <div className='flex gap-[1rem]'>
+         <Image 
+          src="/instagram.svg" 
+          alt='Instagram icon'
+          width={25} 
+          height={25} className='cursor-pointer'/>
+          <Image 
+          src="/facebook.svg" 
+          alt='Facebook icon'
+          width={25} 
+          height={25} className='cursor-pointer'/>
+          <Image 
+          src="/reddit.svg" 
+          alt='Reddit icon'
+          width={25} 
+          height={25} className='cursor-pointer'/>
+         </div>
         </div>
 
-        <Link href="/" className='underline underline-offset-2 mb-4 hover:no-underline'>Cookies Policy</Link>
-        <Link href="/" className='underline underline-offset-2 hover:no-underline'>Privacy Policy</Link>
+        <div className='flex justify-between text-white mt-[7rem]'>
+        
+        <div className='privacy-footer'>
+          <Link href="/" className='privacy-link hover:underline decoration-2 underline-offset-2 mr-8 '>PRIVACY POLICY</Link>
+
+          <Link href="/" className='hover:underline decoration-2 underline-offset-2 '>COOKIE POLICY</Link>
+        </div>
+
+        <div className='back-container'>
+          <Link href="#header" className='inline-flex underline decoration-2 underline-offset-2 gap-[.3rem] hover:no-underline'>
+          <Image 
+          src="/arrow.svg" 
+          alt='Arrow icon'
+          width={15} 
+          height={15} className='cursor-pointer'/> BACK TO TOP</Link>
+        </div>
+
+        </div>
     </footer>
   )
 }
